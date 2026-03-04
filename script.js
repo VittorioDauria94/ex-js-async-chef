93fcec90602d210e81e96e6ae61cc1ce1fda2e76
VittorioDauria94 <dauriavittorio94@gmail.com>
2026-03-04 12:02:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// In questo esercizio, utilizzerai async/await per creare la funzione getChefBirthday(id). Questa funzione accetta un id di una ricetta e deve:
+// Recuperare la ricetta da https://dummyjson.com/recipes/{id}
+// Estrarre la proprietà userId dalla ricetta
+// Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
+// Restituire la data di nascita dello chef
+
+// Note del docente
+// Scrivi la funzione getChefBirthday(id), che deve:
+// Essere asincrona (async).
+// Utilizzare await per chiamare le API.
+// Restituire una Promise con la data di nascita dello chef.
+// Gestire gli errori con try/catch
+
+async function fetchJson(url) {
+  const resp = await fetch(url);
+  return await resp.json();
+}
+
+async function getChefBirthday(id) {
+  const ricetta = await fetchJson(`https://dummyjson.com/recipes/${id}`);
+  const chef = await fetchJson(`https://dummyjson.com/users/${ricetta.userId}`);
+  return chef.birthDate;
+}
+
+(async () => {
+  const chefBirthday = await getChefBirthday(1);
+  console.log(`Data di nascita dello chef: ${chefBirthday}`);
+})();
